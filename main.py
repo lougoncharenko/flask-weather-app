b79120e52036b618177b073f79ddad07093a3c40
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-10 15:22:09 -0500

    imported libraries

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello')
+import os
+import requests
+
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, send_file
+
+app = Flask(__name__)
+
+
+# Get the API key from the '.env' file
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+
